b399dfa4328b40f3caca7707a1ab131825b824f4
ahmet bilgic <ahmet_bilgic@yahoo.com>
2024-06-04 23:59:52 +0300

    Add project files.

---
+++ NET_Core_WebAPI_Kubernetes_Demo/Models/DbContextClass.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NET_Core_WebAPI_Kubernetes_Demo.Models
+{
+    public class DbContextClass : DbContext
+    {
+
+        public DbContextClass(DbContextOptions<DbContextClass>
+options) : base(options)
+        {
+
+        }
+
+        public DbSet<ProductDetails> Products { get; set; }
+    }
+}
---
+++ NET_Core_WebAPI_Kubernetes_Demo/Models/SeedData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NET_Core_WebAPI_Kubernetes_Demo.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new DbContextClass(
+                serviceProvider.GetRequiredService<DbContextOptions<DbContextClass>>()))
+            {
+                if (context.Products.Any())
+                {
+                    return;
+                }
+
+                context.Products.AddRange(
+                    new ProductDetails
+                    {
+                        Id = 1,
+                        ProductName = "IPhone",
+                        ProductDescription = "IPhone 14",
+                        ProductPrice = 120000,
+                        ProductStock = 100
+                    },
+                    new ProductDetails
+                    {
+                        Id = 2,
+                        ProductName = "Samsung TV",
+                        ProductDescription = "Smart TV",
+                        ProductPrice = 400000,
+                        ProductStock = 120
+                    });
+                context.SaveChanges();
+            }
+        }
+    }
+}
